fcfe92d3bfdea48fbccf9bff079e659c1f95b891
Jorge Luiz <jorgeluiz006@hotmail.com>
2024-02-19 12:40:21 -0300

    User insert

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -98,7 +99,8 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-
+	
+	
 	public Set<OrderItem> getItems() {
 		return items;
 	}
